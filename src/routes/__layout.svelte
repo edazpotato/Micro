448994623072fbf6504fb7221c74b8a010588e8f
Edaz <edazpotato@gmail.com>
2021-08-13 12:05:23 +1200

    setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../styles/tailwind-output.css";
+</script>
+
+
+<section>
+  <slot></slot>
+</section>
+
+<style>
+  section {
+    margin: auto 0;
+  }
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["./src/**/*.svelte"],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
