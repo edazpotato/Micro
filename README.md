31e48f4e3ba9162123d0ba9d60281bfcfef31833
Edazpotato <edazpotato@gmail.com>
2021-09-01 11:14:07 +1200

    add emoji support to to plans
    Signed-off-by: Edazpotato <edazpotato@gmail.com>

--- README.md
+++ README.md
@@ -5,8 +5,10 @@
 You can have a look at the designs [here](https://www.figma.com/file/JElZBj1O6KFYTBAfu4zx75/Micro?node-id=0%3A1).
 
 ## Future Plans
-- [ ] [Initial Release](https://github.com/edazpotato/Micro/projects/1)
-- [ ] Supporter System
-- [ ] Image Support
-- [ ] Friend System
-- [ ] Developer Portal
+
+-   [ ] [Initial Release](https://github.com/edazpotato/Micro/projects/1)
+-   [ ] Emoji support
+-   [ ] Supporter System
+-   [ ] Image Support & Custom Emojis for Supporters
+-   [ ] Friend System
+-   [ ] Developer Portal
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,22 +6,23 @@ module.exports = {
 	],
 	darkMode: "media", // or 'false' or 'class'
 	theme: {
-		extend: {
-			colors: {
-				blue: "#23539E",
-				red: "#9E2323",
-				green: "#239E2F",
-				pink: "#9E2348",
+		colors: {
+			tansparent: "transparent",
+			current: "currentColor",
 
-				"background-d": "#111111",
-				"foreground-d": "#222222",
-				"inset-d": "#333333",
-				"border-d": "#444444",
-				"header-icon-d": "#555555",
-				"icon-d": "#888888",
-				"text-d": "#FFFFFF",
-				"placeholder-d": "#777777",
-			},
+			blue: "#23539E",
+			red: "#9E2323",
+			green: "#239E2F",
+			pink: "#9E2348",
+
+			"background-d": "#111111",
+			"foreground-d": "#222222",
+			"inset-d": "#333333",
+			"border-d": "#444444",
+			"header-icon-d": "#555555",
+			"icon-d": "#888888",
+			"text-d": "#FFFFFF",
+			"placeholder-d": "#777777",
 		},
 	},
 	variants: {
