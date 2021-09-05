19834ebb733bbafe23d6ff2b1a0832709897f165
Edazpotato <edazpotato@gmail.com>
2021-09-05 14:59:14 +1200

    clsx
    Signed-off-by: Edazpotato <edazpotato@gmail.com>

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { MouseEvent, ReactChild } from "react";
 
+import clsx from "clsx";
 import { useFocusRing } from "@react-aria/focus";
 
 export interface ButtonProps {
@@ -8,7 +9,6 @@ export interface ButtonProps {
 	disabled?: boolean;
 	children?: ReactChild;
 	colour: "blue" | "grey";
-	// [x: string]: any;
 }
 
 export function Button({
@@ -28,19 +28,22 @@ export function Button({
 			tabIndex={disabled ? -1 : 0}
 			onClick={(e) => !disabled && onPress && onPress(e)}
 			disabled={disabled}
-			className={`MicroButton cursor-default duration-100 ease-in-out flex items-center justify-center text-center font-semibold font-13 min-w-84 outline-none focus:outline-none ${
+			className={clsx(
+				"MicroButton cursor-default duration-100 ease-in-out flex items-center justify-center text-center font-semibold font-13 min-w-84 outline-none focus:outline-none",
 				colour === "blue"
-					? `border rounded-full py-7.5 px-15.5 ${
+					? clsx(
+							"border rounded-full py-7.5 px-15.5",
 							disabled
 								? "bg-inset dark:bg-inset-d  border-border dark:border-border-d text-placeholder dark:text-placeholder-d"
 								: "bg-blue text-white border-tansparent hover:bg-blue-hover active:bg-blue-active"
-					  }`
-					: `p-17.5 w-full bg-inset-d text-placeholder-d ${
-							disabled
-								? ""
-								: "hover:bg-grey-hover active:bg-grey-active"
-					  }`
-			} ${isFocusVisible ? "focus:ring-4" : ""}`}
+					  )
+					: clsx(
+							"p-17.5 w-full bg-inset-d text-placeholder-d",
+							!disabled &&
+								"hover:bg-grey-hover active:bg-grey-active"
+					  ),
+				isFocusVisible && "focus:ring-4"
+			)}
 			{...rest}
 		>
 			{/* <Typography boldness="mediumly bold" largeness="button"> */}
