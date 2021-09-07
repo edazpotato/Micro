2b7a91ddd654135921d433d1176fd0bc2039505e
TheModdedChicken <loganshaw1744@gmail.com>
2021-09-07 15:20:00 +0800

    [Backend] Updated workflow

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,11 +2,13 @@ import mongoose from 'mongoose'
 
 export default class Database {
   constructor() {
-    mongoose.connect(process.env.MONGODB_URI!!, { // @ts-ignore
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: false,
-      family: 4
-    })
+    if (process.env.MONGODB_URI!! !== "false") {
+      mongoose.connect(process.env.MONGODB_URI!!, { // @ts-ignore
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: false,
+        family: 4
+      })
+    }
   }
 }
