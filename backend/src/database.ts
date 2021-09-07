a14d81ca9cf74e3bf934dd2c44792bfa8d80d828
Logan Shaw <50579513+TheModdedChicken@users.noreply.github.com>
2021-09-07 15:37:57 +0800

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 export default class Database {
   constructor() {
+    console.log(process.env.MONGODB_URI!!)
     if (process.env.MONGODB_URI!! !== "false") {
       mongoose.connect(process.env.MONGODB_URI!!, { // @ts-ignore
         useNewUrlParser: true,
