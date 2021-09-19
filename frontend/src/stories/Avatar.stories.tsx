4390f6a784830aec214afdb7d1705ac0b7f04ac8
lerichardson <litbelb1@outlook.com>
2021-09-18 21:06:47 -0700

    fix storybook type errors & linting

--- frontend/src/stories/Avatar.stories.tsx
+++ frontend/src/stories/Avatar.stories.tsx
@@ -4,65 +4,62 @@ import { Meta } from "@storybook/react";
 import React from "react";
 
 export default {
-	component: Avatar,
-	title: "Components/Avatar",
-	// argTypes: { onPress: { action: "click " } },
+  component: Avatar,
+  title: "Components/Avatar",
+  // argTypes: { onPress: { action: "click " } },
 } as Meta;
 
 export const Normal = () => (
-	<>
-		<Card className="p-16">
-			<Avatar
-				name="Edaz"
-				src="https://github.com/edazpotato.png"
-				size="small"
-				dontUseNextImage
-			/>
-			<Avatar
-				name="Edaz"
-				src="https://github.com/edazpotato.png"
-				size="bigger"
-				dontUseNextImage
-			/>
-			<Avatar
-				name="Edaz"
-				src="https://github.com/edazpotato.png"
-				size="humongous"
-				dontUseNextImage
-			/>
-		</Card>
-		<Typography>
-			^ Inside a card so that it can be seen against the correct
-			background.
-		</Typography>
-	</>
+  <>
+    <Card className="p-16">
+      <Avatar
+        name="Edaz"
+        src="https://github.com/edazpotato.png"
+        size="small"
+        dontUseNextImage
+      />
+      <Avatar
+        name="Edaz"
+        src="https://github.com/edazpotato.png"
+        size="bigger"
+        dontUseNextImage
+      />
+      <Avatar
+        name="Edaz"
+        src="https://github.com/edazpotato.png"
+        size="humongous"
+        dontUseNextImage
+      />
+    </Card>
+    <Typography>
+      ^ Inside a card so that it can be seen against the correct background.
+    </Typography>
+  </>
 );
 
 export const MissingSrcAttribute = () => (
-	<>
-		<Card className="p-16">
-			<Avatar name="Edaz" size="small" dontUseNextImage />
-			<Avatar name="Edaz" size="bigger" dontUseNextImage />
-			<Avatar name="Edaz" size="humongous" dontUseNextImage />
-		</Card>
-		<Typography>
-			^ Inside a card so that it can be seen against the correct
-			background.
-		</Typography>
-	</>
+  <>
+    <Card className="p-16">
+      <Avatar name="Edaz" size="small" dontUseNextImage />
+      <Avatar name="Edaz" size="bigger" dontUseNextImage />
+      <Avatar name="Edaz" size="humongous" dontUseNextImage />
+    </Card>
+    <Typography>
+      ^ Inside a card so that it can be seen against the correct background.
+    </Typography>
+  </>
 );
 export const InvalidSrcAttribute = () => (
-	<>
-		<Card className="p-16">
-			<Avatar
-				name="Edaz"
-				src="https://thiswillalwaysfail.edaz.codes/image.png"
-				dontUseNextImage
-			/>
-		</Card>
-		<Typography>
-			^ Inside a card so that it can be seen against the correct
-			background.
-		</Typography>
-	</>
+  <>
+    <Card className="p-16">
+      <Avatar
+        name="Edaz"
+        src="https://thiswillalwaysfail.edaz.codes/image.png"
+        dontUseNextImage
+      />
+    </Card>
+    <Typography>
+      ^ Inside a card so that it can be seen against the correct background.
+    </Typography>
+  </>
 );
--- frontend/src/stories/Card.stories.tsx
+++ frontend/src/stories/Card.stories.tsx
@@ -5,48 +5,52 @@ import { MousePointer as MousePointerIcon } from "react-feather";
 import React from "react";
 
 export default {
-	component: Card,
-	title: "Components/Card",
-	argTypes: { onPress: { action: "click " } },
+  component: Card,
+  title: "Components/Card",
+  argTypes: { onPress: { action: "click " } },
 } as Meta;
 
 export const Basic = () => (
-	<Card className="p-16 flex-col">
-		<Typography>Card with a button</Typography>
-		<Button colour="blue" onPress={() => console.log("Clicked!")}>
-			Button!
-		</Button>
-	</Card>
+  <Card className="p-16 flex-col">
+    <Typography>Card with a button</Typography>
+    <Button
+      colour="blue"
+      onPress={() => console.log("Clicked!")}
+      text={"Button!"}
+    />
+  </Card>
 );
 
 export const Link = () => (
-	<Card
-		className="p-16"
-		link
-		href="https://github.com/edazpotato/Micro"
-		target="_blank"
-	>
-		<Typography>Click me to go to something!</Typography>
-	</Card>
+  <Card
+    className="p-16"
+    link
+    href="https://github.com/edazpotato/Micro"
+    target="_blank"
+  >
+    <Typography>Click me to go to something!</Typography>
+  </Card>
 );
 
 export const PropagtionTesting = () => (
-	<Card
-		className="p-16 flex-col"
-		onPress={() => console.log("Card clicked!")}
-		// link
-		// href="https://github.com/edazpotato/Micro"
-		// target="_blank"
-	>
-		<Typography>Click me!</Typography>
+  <Card
+    className="p-16 flex-col"
+    onPress={() => console.log("Card clicked!")}
+    // link
+    // href="https://github.com/edazpotato/Micro"
+    // target="_blank"
+  >
+    <Typography>Click me!</Typography>
 
-		<Button colour="blue" onPress={() => console.log("Blutton clicked!")}>
-			No, click me!
-		</Button>
+    <Button
+      colour="blue"
+      onPress={() => console.log("Blutton clicked!")}
+      text={"No, click me!"}
+    />
 
-		<IconButton
-			onPress={() => console.log("Icon button clicked!")}
-			icon={MousePointerIcon}
-		/>
-	</Card>
+    <IconButton
+      onPress={() => console.log("Icon button clicked!")}
+      icon={MousePointerIcon}
+    />
+  </Card>
 );
