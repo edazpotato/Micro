87359cbe3db958e5f3b9ecda4dda349d34f46b8a
Edazpotato <edazpotato@gmail.com>
2021-10-03 22:19:29 +1300

    fix linter warnings
    Signed-off-by: Edazpotato <edazpotato@gmail.com>

--- frontend/src/components/MeterBar.tsx
+++ frontend/src/components/MeterBar.tsx
@@ -66,8 +66,10 @@ export function MeterBar({
 			<div className="px-20 flex">
 				<Typography largeness="large">{label}</Typography>
 				<legend className="ml-auto flex space-x-12">
-					{parsedSections.map((section) => (
-						<span className="flex items-center">
+					{parsedSections.map((section, i) => (
+						<span
+							key={section.percentage + "_" + i}
+							className="flex items-center">
 							<svg width="10" height="10" className="mr-2">
 								<rect
 									width="10"
@@ -85,8 +87,9 @@ export function MeterBar({
 				</legend>
 			</div>
 			<div className=" h-50 w-full rounded-medium flex items-center overflow-hidden">
-				{parsedSections.map((section) => (
+				{parsedSections.map((section, i) => (
 					<span
+						key={section.percentage + "_" + i}
 						className="h-full overflow-hidden flex-0 flex flex-nowrap items-center justify-center"
 						style={{
 							backgroundColor: `rgb(${section.backgroundColour[0]}, ${section.backgroundColour[1]}, ${section.backgroundColour[2]})`,
