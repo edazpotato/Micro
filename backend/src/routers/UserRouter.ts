f5f89611fab0d7515f90d21f2cda518affa810f9
Logan Shaw <loganshaw1744@gmail.com>
2021-10-12 12:57:23 +0800

    man

--- backend/src/routers/UserRouter.ts
+++ backend/src/routers/UserRouter.ts
@@ -4,7 +4,7 @@ import { UserModel } from '../models'
 import { UniqueID } from 'nodejs-snowflake'
 import server from '..'
 
-const UserRouter = server.router('/user')
+const UserRouter = server.router('/users')
 
 const usernameRegex = /^(?:[A-Z]|[a-z]|\d|\.|\_|\-){3,20}$/i
 // Retreived from https://www.emailregex.com/ on the 6th of October 2021
